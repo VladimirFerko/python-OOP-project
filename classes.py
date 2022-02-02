49189e63dd095240fa15d85220db76b37d20b5c6
VladimirFerko <vladoferko3@gmail.com>
2022-02-03 00:30:04 +0100

    user choice done

--- classes.py
+++ classes.py
@@ -1,19 +1,25 @@
 
 class Person():
-    def __init__(self, name, surname):
+    def __init__(self, name: str, surname: str):
         self.name = name
         self.surname = surname
-
-    def show(self):
-        print(f'My name is {self.name} {self.surname}')
-
+    
 class Student(Person):
-    def __init__(self, name, surname, grade, course):
+    def __init__(self, name: str, surname: str, grade: int, course: str):
         Person.__init__(self, name, surname)
         self.grade = grade
         self.course = course
 
+    def __str__(self):
+        return f'Name: {self.name} \t Surname: {self.surname}\t Grade: {self.grade} \t Course: {self.course}'
+
 class Professor(Person):
-    def __init__(self, name, surname, course):
+    def __init__(self, name: str, surname: str, course: str):
         super().__init__(name, surname)
         self.course = course
+
+class Course():
+    def __init__(self, name: str, specification: str):
+        self.name = name
+        self.specification = specification
+        self.left_space = 5
--- main.py
+++ main.py
@@ -5,14 +5,20 @@ from datetime import datetime
 # main function
 def main():
 
+    # database connection
+    conn = funcs.make_conn()
 
+    # vars
     continue_var = 'Y'
+    student_arr = list()
+    professor_arr = list()
+    courses_arr = list()
 
     # user interface for adding and showing students
     while continue_var == 'Y':
         print('''Do you want to..
-    1 - add a student
-    2 - add a professor
+    1 - add a student (you can add a student only if course exists)
+    2 - add a professor (The professor can teach only one course)
     3 - add a course
     4 - show students
     5 - show proffesors
@@ -20,11 +26,25 @@ def main():
     7 - exit''')
 
         while True:
+            try:
+                user_choice = int(input('Which option would you like to choose? '))
+                if user_choice > 0 and user_choice < 8:
+                    break
+            except ValueError:
+                print('Int please..')
+
+        vladko = classes.Student('Vladimir','Ferko', 8, 'peniazkovy')
+        romanko = classes.Student('Roman','Fiut', 5, 'robkovy')
+        print(vladko)
+        print(romanko)
+
+        while True:
             continue_var = input('Do you want to continue? [Y/n] ').upper()
             if continue_var == 'Y' or continue_var == 'N':
                 break
 
     print('Goodbye')
+    conn.close()
             
 
 if __name__ == '__main__':
